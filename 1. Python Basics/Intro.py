1c40c40a6c827086c3fc98f9c14343723bc1f32b
tohajan <tohajanlillah@gmail.com>
2024-09-07 11:08:23 -0400

    update file

--- 1. Python Basics/Intro.py
+++ 1. Python Basics/Intro.py
@@ -29,6 +29,38 @@ x = 4
 y = 26
 print(y**x)
 
+print(7+8) #prints out "15"
+print("hello "+ "world") #prints "hello world"
+print(7+"8") #TypeError: unsupported operand type(s) for +: 'int' and 'str'
+print(type("a")) #prints out "<class 'str'>"
+print(type(2)) #prints out "<class 'int'>"
+print(type(2.5)) #prints out "<class 'float'>"
 
 
-#****************************************************BASIC PYTHON SYNTAX****************************************************
+
+#****************VARIABLE ANNOTATION******************************
+name: str="Betty" #the "str" helps to label (annotate) the variable name, i.e., hint that the variable should hold a string value.
+age: int=34 #likewise, "int" hints that the age variable should hold an integer type value
+
+#However, Python is flexible and doesn't require a programmer to declare variable types. That is, the type of variable can change over 
+#..time as new values are assigned to it. This phenomenon is known as dynamic typing (allows programmers to write code more quickly and 
+#..offers flexibility because you don’t have to explicitly declare the type of variable)
+
+#*****************
+print(7+8.5)
+print("a"+"b"+"c")
+print("This " + "is " + "pretty " + "neat!")
+base = 6
+height = 3
+area = (base*height)/2
+print("The area of the triangle is: " + str(area)) #the str() function here converts the numerical object to a string
+
+# PRACTICE QUESTION
+#In this scenario, we have a directory with 5 files in it. Each file has a different size: 2048, 4357, 97658, 125, and 8. Fill in the 
+#..blanks to calculate the average file size by having Python add all the values for you, and then set the files variable to the number 
+#..of files. Finally, output a message saying "The average size is: " followed by the resulting number. Remember to use the str() function 
+#..to convert the number into a string. 
+total = 2048 + 4357 + 97658 + 125 + 8
+files = 5
+average = total / files
+print("The average size is: " + str(average))
