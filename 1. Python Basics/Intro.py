c9c7d38b215990f54c7ca897ba03f964907f3736
tohajan <tohajanlillah@gmail.com>
2024-09-07 06:05:53 -0400

    rename file

---
+++ 1. Python Basics/Intro.py
@@ -0,0 +1,34 @@
+# Skill 1: using the print() function
+print("Hello, World!")
+
+# Syntax for printing a string of text
+print("I love Python!")
+
+# Syntax for printing numeric values
+print(360)
+print(32*45)
+
+# Syntax for printing the value of a variable
+value = 8*6
+print(value)
+
+
+# Skill 2: Using arithmetic operators, with a focus on exponents
+# Multiplication, division, addition, and subtraction
+print(3*8/2+5-1)
+ 
+# Exponents
+print(4**6) # Syntax means 4 to the power of 6
+print(4**2) # To square a number
+print(4**3) # To cube a number
+print(4**0.5) # To find the square root of a number
+
+# To calculate how many different possible combinations can be formed using a set of "x" characters with each character in "x"
+# having "y" number of possible values, you will need to use an exponent for the calculation:
+x = 4
+y = 26
+print(y**x)
+
+
+
+#****************************************************BASIC PYTHON SYNTAX****************************************************
