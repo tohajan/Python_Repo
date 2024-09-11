472dab921c832e703620b3ec81de14d843d30129
tohajan <tohajanlillah@gmail.com>
2024-09-11 10:40:41 -0400

    update file

--- 1. Python Basics/4. WHILE loops.py
+++ 1. Python Basics/4. WHILE loops.py
@@ -19,9 +19,9 @@ attempts(5)
 
 
 #*********************WHY INITIALIZING VARIABLES MATTERS**********************
-while my_variable < 10:
-    print("Hello")
-    my_variable += 1
+# while my_variable < 10:
+#     print("Hello")
+#     my_variable += 1
 #The above code returns an error because my_variable is not defined/initialized. The following fixes the issue:
 my_variable = 5
 while my_variable < 10:
@@ -83,10 +83,10 @@ if x != 0: #Here, the loop is executed only when x is not equal to 0
 while x != 0 and x % 2 == 0: #Here, the loop is exceuted only if the two defined conditions are met
     x = x / 2
 
-while True:
-    do_something_cool()
-    if user_requested_to_stop():
-        break #this breaks an infinite loop
+# while True:
+#     do_something_cool()
+#     if user_requested_to_stop():
+#         break #this breaks an infinite loop
 
 #PRACTICE
 #The following code causes an infinite loop. Can you figure out what’s missing and how to fix it?
@@ -97,3 +97,143 @@ def print_range(start, end):
 		print(n)
 		n+=1 #THIS LINE IS THE FIX. IT WASN'T INCLUDED IN THE ORIGINAL CODE.
 print_range(1, 5)
+
+
+
+# *************** PRACTICE QUIZ**********************
+#1. The following code contains an infinite loop, meaning the Python interpreter does not know when to exit the loop once the task is 
+#..complete. To solve the problem, you will need to:
+#       Find the error in the code
+#       Fix the while loop so there is an exit condition
+
+# def is_power_of_two(number):
+#   # This while loop checks if the "number" can be divided by two without leaving a remainder. How can you change the while loop to
+#   # avoid a Python ZeroDivisionError?
+#   while number % 2 == 0:
+#     number = number / 2
+#   # If after dividing by 2 "number" equals 1, then "number" is a power of 2.
+#   if number == 1:
+#     return True
+#   return False
+# # Calls to the function
+# print(is_power_of_two(0)) # Should be False
+# print(is_power_of_two(1)) # Should be True
+# print(is_power_of_two(8)) # Should be True
+# print(is_power_of_two(9)) # Should be False
+
+# SOLUTION:
+def is_power_of_two(number):
+  if number == 0:
+    return False
+  if number == 1:
+    return True
+  elif number > 1:
+    while number % 2 == 0:
+      number = number / 2
+      if number == 1:
+        return True
+    return False
+# Calls to the function
+print(is_power_of_two(0)) # False
+print(is_power_of_two(1)) # True
+print(is_power_of_two(8)) # True
+print(is_power_of_two(9)) # False
+
+
+# #2. Write a function that takes an argument n and returns the sum of integers from 1 to n. For example, if n=5, your 
+# #..function should add up 1+2+3+4+5 and return 15. If n is less than 1, just return 0. Use a while loop to calculate this sum. 
+# def sum_of_integers(n):
+#   if n < 1:
+#     return 0
+#   i = 1
+#   sum = ___
+#   while ___:
+#     sum = sum + i
+#     i = ___
+#   return sum
+# print(sum_of_integers(3))  # should print 6
+# print(sum_of_integers(4))  # should print 10
+# print(sum_of_integers(5))  # should print 15
+
+#SOLUTION:
+def sum_of_integers(n):
+  if n < 1:
+    return 0
+  i = 1
+  sum = 0
+  while i <= n:
+    sum = sum + i
+    i = i+1
+  return sum
+print(sum_of_integers(3))  # 6
+print(sum_of_integers(4))  # 10
+print(sum_of_integers(5))  # 15
+
+
+#3. Fill in the blanks to complete the function, which should output a multiplication table. The function accepts a 
+#..variable “number” through its parameters. This “number” variable should be multiplied by the numbers 1 through 5, and 
+#..printed in a format similar to “1x6=6” (“number” x “multiplier” = “result”). The code should also limit the “result” 
+#..to not exceed 25. To satisfy these conditions, you will need to:
+#       Initialize the “multiplier" variable with the starting value
+#       Complete the while loop condition
+#       Add an exit point for the loop
+#       Increment the “multiplier" variable inside the while loop
+# def multiplication_table(number):
+#     # Initialize the appropriate variable
+#     ___ = ___
+#     # Complete the while loop condition.
+#     while ___:
+#         result = number * multiplier 
+#         if  result > 25:
+#             # Enter the action to take if the result > 25
+#             ___
+#         print(str(number) + "x" + str(multiplier) + "=" + str(result))
+#         # Increment the appropriate variable
+#         ___ += 1
+# multiplication_table(3) 
+# # Should print: 
+# # 3x1=3 
+# # 3x2=6 
+# # 3x3=9 
+# # 3x4=12 
+# # 3x5=15
+# multiplication_table(5) 
+# # Should print: 
+# # 5x1=5
+# # 5x2=10
+# # 5x3=15
+# # 5x4=20
+# # 5x5=25
+# multiplication_table(8) 
+# # Should print:
+# # 8x1=8
+# # 8x2=16
+# # 8x3=24
+
+# SOLUTION
+def multiplication_table(number):
+    multiplier = 1
+    while multiplier <= 5:
+        result = number * multiplier 
+        if  result > 25:
+            break
+        print(str(number) + "x" + str(multiplier) + "=" + str(result))
+        multiplier += 1
+multiplication_table(3) # prints: 
+#                               3x1=3 
+#                               3x2=6 
+#                               3x3=9 
+#                               3x4=12 
+#                               3x5=15
+
+multiplication_table(5) # prints: 
+#                               5x1=5
+#                               5x2=10
+#                               5x3=15
+#                               5x4=20
+#                               5x5=25
+
+multiplication_table(8) # prints:
+#                               8x1=8
+#                               8x2=16
+#                               8x3=24
