8f55790f946be4beb8aff3d6a26232c1f1265c2a
tohajan <tohajanlillah@gmail.com>
2024-09-12 09:30:50 -0400

    upload new file

---
+++ A. Python Basics/6. Strings.py
@@ -0,0 +1,15 @@
+# -----
+name = "Sasha"
+color = 'Gold'
+# place = "Cambridge' #this line throws an error (only one type of quote [single or double] can be used in a single 
+#..string, not a mixture of both
+pet = "" #a string can also be empty
+
+print("Name: " + name + ", Favorite color: " + color) #concatenation
+"example" * 3 #prints exampleexampleexample
+"example " * 3 #example example example 
+
+pet = "loooooooooooooooooooooooooooooooog cat"
+len(pet)
+
+#---------------------------Parts of a string ---------
