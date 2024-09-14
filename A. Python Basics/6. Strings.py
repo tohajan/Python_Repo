517510726f580ff15199ce0c9ae6316138472e93
tohajan <tohajanlillah@gmail.com>
2024-09-14 07:56:19 -0400

    update file

--- A. Python Basics/6. Strings.py
+++ A. Python Basics/6. Strings.py
@@ -114,3 +114,23 @@ int("12345") + int("54321") #converts numeric strings to integers, which are the
 
 "This is another example".split() #splits the sentence into single-word strings
 #prints "['This', 'is', 'another', 'example']"
+
+
+# ----------------------- Formatting Strings -----------------------------------------------------------
+name = "Manny"
+number = len(name) * 3
+print("Hello {}, your lucky number is {}".format(name, number))
+
+name = "Manny"
+print("Your lucky number is {number}, {name}.".format(name=name, number=len(name)*3))
+
+price = 7.5
+with_tax = price * 1.09
+print(price, with_tax)
+print("Base price: ${:.2f}. With Tax: ${:.2f}".format(price, with_tax))
+
+def to_celsius(x):
+  return (x-32)*5/9
+
+for x in range(0,101,10):
+  print("{:>3} F | {:>6.2f} C".format(x, to_celsius(x)))
